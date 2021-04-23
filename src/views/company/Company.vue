52687d08917a74414b53f9401ee715b6c06a7149
Aliexer <amayor.lu@gmail.com>
2021-04-22 20:35:28 -0500

    Show Customer Portal only if SuperUser

--- src/views/company/Company.vue
+++ src/views/company/Company.vue
@@ -11,6 +11,7 @@
           </p>
           <div
             class="switch-tab flex items-center sm:ml-12 mt-2 sm:mt-0 mb-2 md:mb-0 md:ml-0 ml-4"
+            v-if="isSuperAdmin"
           >
             <div
               class="text-center py-3 px-6 karla"
@@ -36,8 +37,8 @@
       </div>
 
       <div class="page-content">
-        <payment-tab v-if="tab == 'payments'" />
-        <location-tab v-else-if="tab == 'locations'" />
+        <payment-tab v-if="isSuperAdmin && tab == 'payments'" />
+        <location-tab v-else-if="!isSuperAdmin || tab == 'locations'" />
       </div>
     </div>
   </div>
@@ -70,6 +71,7 @@ export default {
   data() {
     return {
       tab: "payments",
+      isSuperAdmin: false,
       companyName: "",
       companyPhone: "",
       companyAddress: "",
@@ -423,6 +425,11 @@ export default {
         this.companyPhone = this.company.phone;
       });
   },
+  mounted(){
+    var superRole = {name:"super admin",key:0};
+    var userRole = JSON.parse(localStorage.getItem("userInfo")).role;
+    this.isSuperAdmin = JSON.stringify(userRole) == JSON.stringify(superRole);
+  }
 };
 </script>
 
