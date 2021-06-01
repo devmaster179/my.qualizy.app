3439fbd9c4746e5fe866496a7c0474473fab8542
devmaster79 <developerwebmaster3@gmail.com>
2021-06-01 13:57:41 +0300

    fix multiple logs by fixing async in log_usages

--- src/views/tasks/TasksAdmin.vue
+++ src/views/tasks/TasksAdmin.vue
@@ -1049,9 +1049,8 @@ export default {
             this.assign(task, unscheduled);
           }
         });
-
     },
-    async assign(task, unscheduled) {
+    assign(task, unscheduled) {
       if (task.assign != undefined) {
         var cUser = this.$store.getters["app/currentUser"];
         var cTeam = cUser.team || [];
