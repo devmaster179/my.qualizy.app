48b611508750ddce07811038bf340c21628c3d4f
devmaster79 <developerwebmaster3@gmail.com>
2021-06-10 21:08:05 +0300

    fix top bar text

--- src/layouts/main/Main.vue
+++ src/layouts/main/Main.vue
@@ -7,16 +7,27 @@
     Author URL: http://www.themeforest.net/user/pixinvent
 ========================================================================================== -->
 
-
 <template>
   <div>
-    <div v-if="!subscribed" class="banner-non-subscribed">
+    <div v-if="!subscribed" class="banner-non-subscribed" id="banner-non-subscribed">
       <span class="info-badge">Info</span>
-      This is a demo account, you can set everything up in Qualizy but will be unable to create records - <a href="https://www.qualizy.app/get-monthly/" target="_blank" class="underline">Unlock an unlimited usage of Qualizy pro </a> for FREE for 7 days today.
+      This is a demo account, you can set everything up in Qualizy but will be
+      unable to create records -
+      <a
+        href="https://www.qualizy.app/get-monthly/"
+        target="_blank"
+        class="underline"
+        >Unlock an unlimited usage of Qualizy pro
+      </a>
+      for FREE for 7 days today.
     </div>
     <div
       class="layout--main"
-      :class="[navbarClasses, footerClasses, { 'app-page': isAppPage, 'layout--main-non-subscribed':!subscribed }]"
+      :class="[
+        navbarClasses,
+        footerClasses,
+        { 'app-page': isAppPage, 'layout--main-non-subscribed': !subscribed },
+      ]"
     >
       <vx-sidebar
         :sidebarItems="sidebarItems"
@@ -780,7 +791,11 @@ export default {
       // GET status of subscription and next billing date
       db.collection("paykickstart_subscriptions")
         // .where("content.buyer_email", "==", "bestsolution2028@gmail.com")
-        .where("user_group", "==", JSON.parse(localStorage.getItem("userInfo")).group)
+        .where(
+          "user_group",
+          "==",
+          JSON.parse(localStorage.getItem("userInfo")).group
+        )
         // .where("content.event", "in", ['subscription-updated', 'subscription-payment', 'subscription-created', 'sales'])
         .orderBy("created_at", "desc")
         .limit(1)
@@ -799,9 +814,9 @@ export default {
           });
           this.$store.dispatch("app/setSubscription", {
             subscribed: subscription.content.event != "subscription-cancelled",
-            invoiceId: subscription.content.invoice_id
+            invoiceId: subscription.content.invoice_id,
           });
-        })
+        });
     },
   },
   components: {
@@ -837,7 +852,7 @@ export default {
     // window.addEventListener('offline',  this.updateStatus('offline'));
   },
   async created() {
-    document.addEventListener("gistChatReady", function () {
+    document.addEventListener("gistChatReady", function() {
       window.gist.chat("hideLauncher");
     });
     // this.deleteTrashedData()
@@ -998,20 +1013,18 @@ export default {
 }
 </style>
 <style>
-
 .layout--main-non-subscribed .vs-sidebar {
-  top: 32px!important;
+  top: 0px;
 }
 .layout--main-non-subscribed .main-layout-navbar .vx-navbar-wrapper {
-  top: 32px!important;
+  top: 0px;
 }
 .layout--main-non-subscribed #content-area {
   position: relative;
-  top: 32px!important;
+  top: 0px;
 }
 .banner-non-subscribed {
   background-color: #fffbcc;
-  height: 32px;
   padding: 5px;
   text-align: center;
   font-size: 13px;
