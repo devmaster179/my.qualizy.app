466008661e3dc126620824a3f706639617c5be64
devmaster79 <developerwebmaster3@gmail.com>
2021-07-19 00:49:41 +0300

    improve performance by fixing GTM

--- src/views/template-edit/TemplateEdit.vue
+++ src/views/template-edit/TemplateEdit.vue
@@ -184,6 +184,7 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import TemplatemakingPopup from "../template-making/TemplatemakingPopup";
 
 import VuePerfectScrollbar from "vue-perfect-scrollbar";
@@ -193,6 +194,19 @@ import { db } from "@/firebase/firebaseConfig";
 
 import firebase from "firebase/app";
 import "@firebase/firestore";
+import VueGtm from 'vue-gtm';
+import router from '/src/router.js';
+
+Vue.use(VueGtm, {
+  id: 'GTM-TMSFKHD', // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
+  defer: false, // defaults to false. Script can be set to `defer` to increase page-load-time at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible)
+  enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+  debug: false, // Whether or not display console logs debugs (optional)
+  loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+  vueRouter: router, // Pass the router instance to automatically sync with router (optional)
+  ignoredViews: [], // Don't trigger events for specified router names (case insensitive) (optional)
+  trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
+});
 
 export default {
   components: {
